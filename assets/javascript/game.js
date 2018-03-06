f101e8587fba74fcd003d233f98047ca808e975b
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-06 11:36:54 -0500

    validate html, css, and format code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 //Global variables
 var stateOfGame = 1;
 /* values of StateofGame:
@@ -16,12 +15,12 @@ var LevelValue = 5;
 function Character() {
     this.id = "";
     this.name = "";
-    this.imgRef =  "";
+    this.imgRef = "";
     this.classes = "";
-    this.maxHitPoints= 0;
-    this.currentHitPoints= 0;
-    this.level= 0;
-    this.attackDice= 0;
+    this.maxHitPoints = 0;
+    this.currentHitPoints = 0;
+    this.level = 0;
+    this.attackDice = 0;
 
     this.takeDamage = function (amount) {
         this.currentHitPoints -= amount;
@@ -68,42 +67,41 @@ maul.currentHitPoints = maul.maxHitPoints;
 maul.level = 0;
 maul.attackDice = 12;
 
-$(document).ready(function() {
-    $("#battlereport").html ("Select your character: ");
+$(document).ready(function () {
+    $("#battlereport").html("Select your character: ");
     $("#playercontainer").hide();
     $("#opponentcontainer").hide();
-    
 });
 
-function addCharacter(CharacterObject, AreaId){
+function addCharacter(CharacterObject, AreaId) {
     var holder = document.createElement("div");
-    $(holder).attr("id",CharacterObject.id);
+    $(holder).attr("id", CharacterObject.id);
     $(holder).addClass("character");
-    $(holder).html ("<img src=" + CharacterObject.imgRef + " alt=" + CharacterObject.name +" />");
-    
+    $(holder).html("<img src=" + CharacterObject.imgRef + " alt=" + CharacterObject.name + " />");
+
     var nextHolder = document.createElement("div");
     $(nextHolder).addClass("hitpoints hitpointscombat");
-    $(nextHolder).html ('<h5>' + CharacterObject.name + '</h5><br>' +  "<h5>Hit points:");
-    
+    $(nextHolder).html('<h5>' + CharacterObject.name + '</h5><br>' + "<h5>Hit points:");
+
     var nextNextHolder = document.createElement("span")
-    $(nextNextHolder).attr("id",CharacterObject.id);
-    $(nextNextHolder).addClass("badge badge-secondary subtitle " + CharacterObject.id  + "currenthitpoints")
-    $(nextNextHolder).html ("<h5>" + CharacterObject.currentHitPoints + "</h5>");
-    
+    $(nextNextHolder).attr("id", CharacterObject.id);
+    $(nextNextHolder).addClass("badge badge-secondary subtitle " + CharacterObject.id + "currenthitpoints")
+    $(nextNextHolder).html("<h5>" + CharacterObject.currentHitPoints + "</h5>");
+
     $(nextHolder).append(nextNextHolder);
-     $(holder).append(nextHolder);
+    $(holder).append(nextHolder);
     $("#" + AreaId).append(holder);
 }
 
-function rollDice(D){ //generates and returns a random number from 1 to D
+function rollDice(D) { //generates and returns a random number from 1 to D
     var diceRoll = Math.floor(Math.random() * D) + 1;
     return diceRoll;
 }
 
-$(".character").click (function() { /*captures when a character is clicked */
+$(".character").click(function () { /*captures when a character is clicked */
     switch (stateOfGame) {
-    case 1: //selecting playerCharacter
-            switch (this.id){
+        case 1: //selecting playerCharacter
+            switch (this.id) {
                 case "luke":
                     playerCharacter = luke;
                     break;
@@ -119,14 +117,14 @@ $(".character").click (function() { /*captures when a character is clicked */
             }
             $("#" + this.id).remove();
             $("#playercontainer").fadeIn();
-            addCharacter (playerCharacter,"playercontainer");
+            addCharacter(playerCharacter, "playercontainer");
             stateOfGame = 2;
-            $("#battlereport").html ("Select an opponent: ");
+            $("#battlereport").html("Select an opponent: ");
             break;
-            
-    case 2: //selecting opponentCharacter
-        if (this.id !== playerCharacter.id){
-                switch (this.id){
+
+        case 2: //selecting opponentCharacter
+            if (this.id !== playerCharacter.id) {
+                switch (this.id) {
                     case "luke":
                         opponentCharacter = luke;
                         break;
@@ -141,50 +139,51 @@ $(".character").click (function() { /*captures when a character is clicked */
                         break;
                 }
                 $("#" + this.id).remove();
-                if (opponentsDefeated >= 2){ $("#startcontainer").fadeOut() }
+                if (opponentsDefeated >= 2) {
+                    $("#startcontainer").fadeOut()
+                }
                 $("#opponentcontainer").fadeIn();
-                addCharacter (opponentCharacter,"opponentcontainer");
+                addCharacter(opponentCharacter, "opponentcontainer");
                 stateOfGame = 3; //go to combat mode
-                $("#battlereport").html ("Ready for combat!");
+                $("#battlereport").html("Ready for combat!");
                 break;
             }
-        }
-}); 
+    }
+});
 
-$("#attack").click (function() { // Captures the user clicking the attack button
-    if(stateOfGame === 3){
+$("#attack").click(function () { // Captures the user clicking the attack button
+    if (stateOfGame === 3) {
+        var myElement = document.getElementById("combat");
+        myElement.play(); //plays lightsaber noise
         var playerDamage = rollDice(opponentCharacter.attackDice);
         var opponentDamage = (LevelValue * playerCharacter.level) + rollDice(playerCharacter.attackDice);
         playerCharacter.takeDamage(playerDamage);
         opponentCharacter.takeDamage(opponentDamage);
         playerCharacter.level++; //'<h5>' + CharacterObject.currentHitPoints + '</h5>
-        $("." + playerCharacter.id  + "currenthitpoints").html ('<h5>' + playerCharacter.currentHitPoints + '</h5>');
-        $("." + opponentCharacter.id  + "currenthitpoints").html ('<h5>' + opponentCharacter.currentHitPoints + '</h5>');
-        $("#battlereport").html ('<p>Battle report:</p> You received ' + playerDamage + ' damage. <br><br><p> Your opponent received ' + opponentDamage + '.'); 
-            if (playerCharacter.currentHitPoints <= 0){
-                stateOfGame = 4;
-                $("#battlereport").html ("You have been defeated");
+        $("." + playerCharacter.id + "currenthitpoints").html('<h5>' + playerCharacter.currentHitPoints + '</h5>');
+        $("." + opponentCharacter.id + "currenthitpoints").html('<h5>' + opponentCharacter.currentHitPoints + '</h5>');
+        $("#battlereport").html('<p>Battle report:</p> You received ' + playerDamage + ' damage. <br><br><p> Your opponent received ' + opponentDamage + '.');
+        if (playerCharacter.currentHitPoints <= 0) {
+            stateOfGame = 4;
+            $("#battlereport").html("You have been defeated");
+        } else if (opponentCharacter.currentHitPoints <= 0) {
+            $("#opponentcontainer").hide();
+            $("#" + opponentCharacter.id).remove();
+            opponentsDefeated++;
+            if (opponentsDefeated === 3) {
+                stateOfGame = 5;
+                $("#battlereport").html("You won!");
+            } else {
+                $("#opponentcontainer").hide();
+                $("#battlereport").append("<p> and you have defeated " + opponentCharacter.name + "!</p>");
+                stateOfGame = 2;
             }
-            else if (opponentCharacter.currentHitPoints <= 0) {
-                $("#opponentcontainer").hide();
-                $("#" + opponentCharacter.id).remove();
-                opponentsDefeated++;
-                if (opponentsDefeated === 3){
-                    stateOfGame = 5;
-                    $("#battlereport").html ("You won!");            
-                }
-            else {
-                $("#opponentcontainer").hide();
-                $("#battlereport").append ("<p> and you have defeated "+ opponentCharacter.name + "!</p>");
-                stateOfGame = 2;            
-            }            
         }
-    }
-    else if(stateOfGame=== 2) {
-        $("#battlereport").html ("Select an opponent: ");
+    } else if (stateOfGame === 2) {
+        $("#battlereport").html("Select an opponent: ");
     }
 });
 
-$("#reset").click(function() { // captures the user clicking the reset button
+$("#reset").click(function () { // captures the user clicking the reset button
     location.reload();
 });
